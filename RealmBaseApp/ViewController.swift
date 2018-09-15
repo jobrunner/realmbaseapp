d6e741db8bb4fa1a5ed6adcc34686db06e9d547b
Jo Brunner <jo.brunner@mayflower.de>
2018-09-15 10:17:47 +0200

    Inital commit

---
+++ RealmBaseApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RealmBaseApp
+//
+//  Created by Jo Brunner on 15.09.18.
+//  Copyright © 2018 Mayflower GmbH. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
